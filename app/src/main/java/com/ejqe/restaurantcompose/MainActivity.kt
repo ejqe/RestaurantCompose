ccca4d0e0f97aa92f866e4144ce8ff5dd266e6a3
ejqe <erickeugenio218@gmail.com>
2023-04-06 23:14:39 +0800

    initial commit

---
+++ app/src/main/java/com/ejqe/restaurantcompose/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.ejqe.restaurantcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.ejqe.restaurantcompose.ui.theme.RestaurantComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RestaurantComposeTheme {
+                RestaurantsScreen()
+            }
+        }
+    }
+}
+
+
+
+
