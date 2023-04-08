ca2dc2ec9b31fa709d5ae9fa0455a8ff8c3c11ec
ejqe <erickeugenio218@gmail.com>
2023-04-08 20:51:25 +0800

    add favorites

--- app/src/main/java/com/ejqe/restaurantcompose/RestaurantsScreen.kt
+++ app/src/main/java/com/ejqe/restaurantcompose/RestaurantsScreen.kt
@@ -1,6 +1,7 @@
 package com.ejqe.restaurantcompose
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -9,9 +10,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Place
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -22,6 +27,7 @@ import com.ejqe.restaurantcompose.ui.theme.RestaurantComposeTheme
 
 @Composable
 fun RestaurantsScreen() {
+//    val viewModel: RestaurantsViewModel = viewModel()
     LazyColumn(
         contentPadding = PaddingValues(
             vertical = 8.dp,
@@ -51,8 +57,25 @@ fun RestaurantItem(item: Restaurant) {
                 Modifier.weight(0.15f)
             )
             RestaurantDetails(item.title, item.description, Modifier.weight(0.85f))
+            FavoriteIcon(Modifier.weight(0.15f))
         }
     }
+}
+
+@Composable
+private fun FavoriteIcon(modifier: Modifier) {
+    val favoriteState = remember { mutableStateOf(false) }
+    val icon = if (favoriteState.value) Icons.Filled.Favorite
+        else Icons.Filled.FavoriteBorder
+    Image(
+        imageVector = icon,
+        contentDescription = "Favorite Restaurant Icon",
+        modifier = modifier
+            .padding(8.dp)
+            .clickable { favoriteState.value = !favoriteState.value }
+    )
+
+
 }
 
 @Composable
---
+++ app/src/main/java/com/ejqe/restaurantcompose/RestaurantsViewModel.kt
@@ -0,0 +1,7 @@
+package com.ejqe.restaurantcompose
+
+import androidx.lifecycle.ViewModel
+
+class RestaurantsViewModel (): ViewModel() {
+    fun getRestaurants() = dummyRestaurants
+}
