8f1cdc3e0cbcd2dc38ffc7397bb35c527715c096
ejqe <erickeugenio218@gmail.com>
2023-04-08 21:29:27 +0800

    state hoisted to RestaurantScreen

--- app/src/main/java/com/ejqe/restaurantcompose/RestaurantsScreen.kt
+++ app/src/main/java/com/ejqe/restaurantcompose/RestaurantsScreen.kt
@@ -13,37 +13,46 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Place
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.ejqe.restaurantcompose.ui.theme.RestaurantComposeTheme
 
 
 @Composable
 fun RestaurantsScreen() {
-//    val viewModel: RestaurantsViewModel = viewModel()
+    val viewModel: RestaurantsViewModel = viewModel()
+    val state: MutableState<List<Restaurant>> = remember {
+        mutableStateOf(viewModel.getRestaurants())
+    }
     LazyColumn(
         contentPadding = PaddingValues(
             vertical = 8.dp,
             horizontal = 8.dp
         )
     ) {
-        items(dummyRestaurants) { restaurant ->
-            RestaurantItem(restaurant)
+        items(state.value) { restaurant ->
+            RestaurantItem(restaurant) { id ->
+                val restaurants = state.value.toMutableList()
+                val itemIndex = restaurants.indexOfFirst { it.id == id }
+                val item = restaurants[itemIndex]
+                restaurants[itemIndex] =
+                    item.copy(isFavorite = !item.isFavorite)
+                state.value = restaurants
+
+            }
         }
     }
 }
 
 @Composable
-fun RestaurantItem(item: Restaurant) {
-    val favoriteState = remember { mutableStateOf(false) }
-    val icon =  if (favoriteState.value) Icons.Filled.Favorite
+fun RestaurantItem(item: Restaurant, onClick: (id: Int) -> Unit) {
+
+    val icon =  if (item.isFavorite) Icons.Filled.Favorite
                 else Icons.Filled.FavoriteBorder
     Card(
         elevation = 4.dp,
@@ -61,7 +70,7 @@ fun RestaurantItem(item: Restaurant) {
             )
             RestaurantDetails(item.title, item.description, Modifier.weight(0.85f))
             RestaurantIcon(icon,Modifier.weight(0.15f))
-                { favoriteState.value = !favoriteState.value }
+                { onClick(item.id) }
         }
     }
 }
