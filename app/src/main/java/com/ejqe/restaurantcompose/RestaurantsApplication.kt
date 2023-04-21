526f029129983d9ae5e65fbf820a58eb36f4f8b6
ejqe <erickeugenio218@gmail.com>
2023-04-21 22:28:19 +0800

    caching to room database

---
+++ app/src/main/java/com/ejqe/restaurantcompose/RestaurantsApplication.kt
@@ -0,0 +1,12 @@
+package com.ejqe.restaurantcompose
+
+import android.app.Application
+import android.content.Context
+
+class RestaurantsApplication: Application() {
+    init { app = this }
+    companion object {
+        private lateinit var app: RestaurantsApplication
+        fun getAppContext(): Context = app.applicationContext
+    }
+}
